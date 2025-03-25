8e62580dba9730a01d443b4b45ee44017db5f421
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-03-25 11:21:13 +0530

    refactor: update audio sources to use external links and remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -11,35 +11,16 @@ import casinoSvg from "./assets/svg/casino.svg";
 import fireworkSvg from "./assets/svg/firework.svg";
 import coffeeSvg from "./assets/svg/coffee.svg";
 import highwaySvg from "./assets/svg/highway.svg";
-import githubSvg from "./assets/svg/github.svg";
+// import githubSvg from "./assets/svg/github.svg";
 
 import pianoSvg from "./assets/svg/piano.svg";
 import acousticSvg from "./assets/svg/acoustic-guitar.svg";
 import spanishSvg from "./assets/svg/spanish-guitar.svg";
 import violinSvg from "./assets/svg/violin.svg";
 import fluteSvg from "./assets/svg/flute.svg";
-import drummerSetSvg from "./assets/svg/drummer-set.svg";
+// import drummerSetSvg from "./assets/svg/drummer-set.svg";
 
-import rainMp3 from "./assets/audio/rain.mp3";
-import wavesMp3 from "./assets/audio/waves.mp3";
-import lofiMp3 from "./assets/audio/lofi.mp3";
-import campfireMp3 from "./assets/audio/campfire.mp3";
-import windMp3 from "./assets/audio/wind.mp3";
-import forestMp3 from "./assets/audio/forest.mp3";
 
-import playgroundMp3 from "./assets/audio/playground.mp3";
-import carnivalMp3 from "./assets/audio/carnival.mp3";
-import casinoMp3 from "./assets/audio/casino.mp3";
-import fireworksMp3 from "./assets/audio/fireworks.mp3";
-import coffeeMp3 from "./assets/audio/coffee.mp3";
-import highwayMp3 from "./assets/audio/highway.mp3";
-
-import beethovenMp3 from "./assets/audio/beethoven.mp3";
-import guitarMp3 from "./assets/audio/guitar-summer-walk.mp3";
-import spanishMp3 from "./assets/audio/spanish-the-lively-dancer.mp3";
-import violinMp3 from "./assets/audio/bach-violin-concerto-in-a-minor-3-movement.mp3";
-import fluteMp3 from "./assets/audio/indian-flute.mp3";
-import drumsMp3 from "./assets/audio/drums.mp3";
 
 import bg from "./assets/bg.png";
 import music1Svg from "./assets/svg/music-1.svg";
@@ -74,30 +55,30 @@ function App() {
       </div>
       <Title title="NATURE" />
       <div className="grid grid-cols-2 gap-10 sm:my-10 sm:grid-cols-3 sm:gap-30 md:gap-24 protest-strike-regular">
-        <Card icon={rainSvg} name={"Rain"} audio={rainMp3} />
-        <Card icon={musicSvg} name={"Lo-fi Beats"} audio={lofiMp3} />
-        <Card icon={wavesSvg} name={"Waves"} audio={wavesMp3} />
-        <Card icon={campfireSvg} name={"Campfire"} audio={campfireMp3} />
-        <Card icon={windSvg} name={"Wind"} audio={windMp3} />
-        <Card icon={forestSvg} name={"Forest"} audio={forestMp3} />
+        <Card icon={rainSvg} name={"Rain"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742648755/rain_zx8zgj.mp3'} />
+        <Card icon={musicSvg} name={"Lo-fi Beats"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649057/lofi_zy5iel.mp3'} />
+        <Card icon={wavesSvg} name={"Waves"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742648831/waves_ergw0t.mp3'} />
+        <Card icon={campfireSvg} name={"Campfire"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649262/campfire_bowe0p.mp3'} />
+        <Card icon={windSvg} name={"Wind"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649363/wind_iyz6nt.mp3'} />
+        <Card icon={forestSvg} name={"Forest"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649483/forest_loqygo.mp3'} />
       </div>
       <Title title="MUSIC INSTRUMENTS" />
       <div className="grid grid-cols-2 gap-10 sm:my-10 sm:grid-cols-3 sm:gap-30 md:gap-24 protest-strike-regular">
-        <Card icon={pianoSvg} name={"Piano"} audio={beethovenMp3} />
-        <Card icon={acousticSvg} name={"Acoustic Guitar"} audio={guitarMp3} />
-        <Card icon={spanishSvg} name={"Spanish Guitar"} audio={spanishMp3} />
-        <Card icon={violinSvg} name={"Violin"} audio={violinMp3} />
-        <Card icon={fluteSvg} name={"Flute"} audio={fluteMp3} />
-        <Card icon={drummerSetSvg} name={"Drums"} audio={drumsMp3} />
+        <Card icon={pianoSvg} name={"Piano"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650177/beethoven_ffcaml.mp3'} />
+        <Card icon={acousticSvg} name={"Acoustic Guitar"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650216/guitar-summer-walk_lzus0t.mp3'} />
+        <Card icon={spanishSvg} name={"Spanish Guitar"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650237/spanish-the-lively-dancer_dulysn.mp3'} />
+        <Card icon={violinSvg} name={"Violin"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650315/wind_xty2qb.mp3'} />
+        <Card icon={fluteSvg} name={"Flute"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650343/indian-flute_eqdqqa.mp3'} />
+        {/* <Card icon={drummerSetSvg} name={"Drums"} audio={drumsMp3} /> */}
       </div>
       <Title title="OTHER" />
       <div className="grid grid-cols-2 gap-10 sm:my-10 sm:grid-cols-3 sm:gap-30 md:gap-24 protest-strike-regular">
-        <Card icon={playgroundSvg} name={"Playground"} audio={playgroundMp3} />
-        <Card icon={circusSvg} name={"Circus"} audio={carnivalMp3} />
-        <Card icon={casinoSvg} name={"Casino"} audio={casinoMp3} />
-        <Card icon={fireworkSvg} name={"Firework"} audio={fireworksMp3} />
-        <Card icon={coffeeSvg} name={"Coffee Shop"} audio={coffeeMp3} />
-        <Card icon={highwaySvg} name={"Highway"} audio={highwayMp3} />
+        <Card icon={playgroundSvg} name={"Playground"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649787/playground_ob7pbe.mp3'} />
+        <Card icon={circusSvg} name={"Circus"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649845/carnival_dvfivk.mp3'} />
+        <Card icon={casinoSvg} name={"Casino"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742649900/casino_blxw4z.mp3'} />
+        <Card icon={fireworkSvg} name={"Firework"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650062/fireworks_dno63o.mp3'} />
+        <Card icon={coffeeSvg} name={"Coffee Shop"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650109/coffee_i5y1qh.mp3'} />
+        <Card icon={highwaySvg} name={"Highway"} audio={'https://res.cloudinary.com/dfcqmkj0k/video/upload/v1742650141/highway_hbyidx.mp3'} />
       </div>
 
       {!hasInteracted && (
@@ -138,7 +119,7 @@ function App() {
         </div>
       )}
 
-      <div className="z-[1] py-10">
+      {/* <div className="z-[1] py-10">
         <a href="https://github.com/Yagnik-Gohil/Ambient-Chaos" target="_blank">
           <img
             src={githubSvg}
@@ -146,7 +127,7 @@ function App() {
             className="w-16 h-16 mb-4 filter invert"
           />
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
